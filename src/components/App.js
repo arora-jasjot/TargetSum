2e45846385b75b677fcdf4ffcbc1ff3357f3b954
arora-jasjot <jasjotsinghchugh@gmail.com>
2022-08-06 22:25:40 +0530

    Initialized App

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { View, Text, StyleSheet } from 'react-native'
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#ddd',
+    flex: 1
+  }
+})
